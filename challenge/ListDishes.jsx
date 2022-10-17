ccffdfed80563a65a025af2b1bde8a9f091b0e89
PJSalter <45575016+PJSalter@users.noreply.github.com>
2022-10-17 22:41:10 +0100

    added a filter function to show only the prices lower or equal to the max

--- challenge/ListDishes.jsx
+++ challenge/ListDishes.jsx
@@ -1,15 +1,17 @@
 import dishes from "./data.js";
 
-export default function ListDishes() {
+export default function ListDishes({ max }) {
   return (
     <ul className="grid">
-      {dishes.map((dish) => (
-        <li key={dish.id} className="card">
-          <h3>{dish.name}</h3>
-          {dish.description && <p>{dish.description}</p>}
-          <div>£{dish.price.toFixed(2)}</div>
-        </li>
-      ))}
+      {dishes
+        .filter((dish) => dish.price <= max)
+        .map((dish) => (
+          <li key={dish.id} className="card">
+            <h3>{dish.name}</h3>
+            {dish.description && <p>{dish.description}</p>}
+            <div>£{dish.price.toFixed(2)}</div>
+          </li>
+        ))}
     </ul>
   );
 }
